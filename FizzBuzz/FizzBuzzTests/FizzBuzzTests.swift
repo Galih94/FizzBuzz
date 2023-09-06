cfb7ffa231c5afc0ae3a833997dd756b5db7f2a3
galih <samudragalih94@gmail.com>
2023-09-06 21:12:59 +0700

    feat(FizzBuzz): create numberFizz

--- FizzBuzz/FizzBuzzTests/FizzBuzzTests.swift
+++ FizzBuzz/FizzBuzzTests/FizzBuzzTests.swift
@@ -11,11 +11,19 @@ public class FizzBuzz {
     public func numberDefaultText(_ number: Int) -> String {
         return "\(number)"
     }
+    
+    public func numberFizz(_ number: Int) -> String? {
+        return nil
+    }
 }
 
 final class FizzBuzzTests: XCTestCase {
     func test_numberDefaultText_resultNumberInString() {
         XCTAssertEqual(FizzBuzz().numberDefaultText(0), "0")
         XCTAssertEqual(FizzBuzz().numberDefaultText(1), "1")
+    }
+    
+    func test_numberFizz_resultNumberNotMultiplyThree_numberTwo() {
+        XCTAssertEqual(FizzBuzz().numberFizz(2), nil)
     }
 }
