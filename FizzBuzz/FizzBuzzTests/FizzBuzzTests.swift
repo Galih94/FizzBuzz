3e9f1d684de9ffce4735518eef27a52f02a05def
galih <samudragalih94@gmail.com>
2023-09-06 21:08:37 +0700

    refactor(FizzBuzz): refactor tests numberDefaultText

--- FizzBuzz/FizzBuzzTests/FizzBuzzTests.swift
+++ FizzBuzz/FizzBuzzTests/FizzBuzzTests.swift
@@ -14,15 +14,8 @@ public class FizzBuzz {
 }
 
 final class FizzBuzzTests: XCTestCase {
-    func test_numberDefaultText_resultNumberInString_numberZero() {
-        let sut = FizzBuzz()
-        let result = sut.numberDefaultText(0)
-        XCTAssertEqual(result, "0")
-    }
-    
-    func test_numberDefaultText_resultNumberInString_numberOne() {
-        let sut = FizzBuzz()
-        let result = sut.numberDefaultText(1)
-        XCTAssertEqual(result, "1")
+    func test_numberDefaultText_resultNumberInString() {
+        XCTAssertEqual(FizzBuzz().numberDefaultText(0), "0")
+        XCTAssertEqual(FizzBuzz().numberDefaultText(1), "1")
     }
 }
