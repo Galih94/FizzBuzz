101dc068460913c8b60fa76d59008433abafa3de
galih <samudragalih94@gmail.com>
2023-09-06 20:51:38 +0700

    create(FizzBuzz): create project FizzBuzz

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.h
@@ -0,0 +1,18 @@
+//
+//  FizzBuzz.h
+//  FizzBuzz
+//
+//  Created by Galih Samudra on 06/09/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FizzBuzz.
+FOUNDATION_EXPORT double FizzBuzzVersionNumber;
+
+//! Project version string for FizzBuzz.
+FOUNDATION_EXPORT const unsigned char FizzBuzzVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FizzBuzz/PublicHeader.h>
+
+
