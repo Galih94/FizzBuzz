a70b96d08052b0ba04601d1d1c4ae0005e234231
galih <samudragalih94@gmail.com>
2023-09-06 21:07:19 +0700

    feat(FizzBuzz): make numberDefaultText return number in string

--- FizzBuzz/FizzBuzzTests/FizzBuzzTests.swift
+++ FizzBuzz/FizzBuzzTests/FizzBuzzTests.swift
@@ -9,7 +9,7 @@ import XCTest
 
 public class FizzBuzz {
     public func numberDefaultText(_ number: Int) -> String {
-        return "0"
+        return "\(number)"
     }
 }
 
@@ -18,5 +18,11 @@ final class FizzBuzzTests: XCTestCase {
         let sut = FizzBuzz()
         let result = sut.numberDefaultText(0)
         XCTAssertEqual(result, "0")
+    }
+    
+    func test_numberDefaultText_resultNumberInString_numberOne() {
+        let sut = FizzBuzz()
+        let result = sut.numberDefaultText(1)
+        XCTAssertEqual(result, "1")
     }
 }
