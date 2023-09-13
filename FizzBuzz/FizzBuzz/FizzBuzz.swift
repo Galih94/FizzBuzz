a0d8b1aaa79a12a418aac3df7ad3e01cd8259d33
galih <samudragalih94@gmail.com>
2023-09-13 08:48:31 +0700

    refactor(FizzBuzz): move result to enum

--- FizzBuzz/FizzBuzz/FizzBuzz.swift
+++ FizzBuzz/FizzBuzz/FizzBuzz.swift
@@ -10,6 +10,12 @@ public final class FizzBuzz {
     private static let BUZZ_NUMBER = 5
     private static let FIZZBUZZ_NUMBER = 15
     
+    private enum Result: String {
+        case fizz = "Fizz"
+        case buzz = "Buzz"
+        case fizzBuzz = "FizzBuzz"
+    }
+    
     public static func run(from start: Int, to end: Int) -> [String] {
         var result = [String]()
         for i in start...end {
@@ -36,16 +42,16 @@ public final class FizzBuzz {
     
     public static func numberFizz(_ number: Int) -> String? {
         guard number.isMultiple(of: FIZZ_NUMBER) else { return nil }
-        return "Fizz"
+        return Result.fizz.rawValue
     }
     
     public static func numberBuzz(_ number: Int) -> String? {
         guard number.isMultiple(of: BUZZ_NUMBER) else { return nil }
-        return "Buzz"
+        return Result.buzz.rawValue
     }
     
     public static func numberFizzBuzz(_ number: Int) -> String? {
         guard number.isMultiple(of: FIZZBUZZ_NUMBER) else { return nil }
-        return "FizzBuzz"
+        return Result.fizzBuzz.rawValue
     }
 }
